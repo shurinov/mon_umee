2098c8940bac9b23a8731e1abf53a2a3c7707c82
shurinov <@shurinov>
2022-10-20 22:18:03 +0000

    Add PFD metrics, change bonded field

--- monitor.sh
+++ monitor.sh
@@ -3,6 +3,9 @@ pushd `dirname ${0}` >/dev/null || exit 1
 
 # Get node variables
 source ./mon_var.sh
+# Set default curl timeout
+TO=2
+
 # Get timestamp
 now=$(date +%s%N)
 
@@ -23,7 +26,7 @@ then
     echo $logentry" health=$health $now"
 else
     # Get node status
-    status=$(curl -s localhost:$COS_PORT_RPC/status)
+    status=$(curl --connect-timeout ${TO} -s ${NODE_RPC}/status)
     if [ -z "$status" ];
     then
         echo "ERROR: can't connect to UMEE RPC">&2 ;
@@ -41,64 +44,95 @@ else
 
         # Get catchup status
         catching_up=$(jq -r '.result.sync_info.catching_up' <<<$status)
-        # Get Tendermint votiong power
+        # Get Tendermint voting power
         voting_power=$(jq -r '.result.validator_info.voting_power' <<<$status)
         # Peers count
-        peers_num=$(curl -s localhost:${COS_PORT_RPC}/net_info | jq -r '.result.n_peers')
+        peers_num=$(curl --connect-timeout ${TO} -s ${NODE_RPC}/net_info | jq -r '.result.n_peers')
         # Prepare metiric to out
         logentry=$logentry" ver=\"$version\",block_height=$block_height,catching_up=$catching_up,time_since_block=$time_since_block,latest_block_time=$latest_block_time,peers_num=$peers_num,voting_power=$voting_power"
-        
         # Common validator statistic
-        list_limit=3000
         # Numbers of active validators
-        val_active_numb=$(${COS_BIN_NAME} q staking validators -o json --limit=${list_limit} --node "tcp://localhost:${COS_PORT_RPC}" |\
+        list_limit=3000
+        val_active_numb=$(${COS_BIN_NAME} q staking validators -o json --limit=${list_limit} --node "${NODE_RPC}" |\
         jq '.validators[] | select(.status=="BOND_STATUS_BONDED")' | jq -r ' .description.moniker' | wc -l)
         logentry="$logentry,val_active_numb=$val_active_numb"
-				
-				if [ $MON_MODE == "rpc" ]
-				then
-				    health=100 # Health RPC mode code				
-				else 
-            eth_height=$(echo "ibase=16; $(echo $(curl -s -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":83}' $ETH_NODE_RPC | jq -r '.result' | cut -c 3-) | tr [:lower:] [:upper:])" | bc)
+
+        if [ $MON_MODE == "rpc" ]
+        then
+            health=100 # Health RPC mode code
+        else 
+            #
+            # Peggo metrics
+            #
+            # Ethereum node latest block height
+            eth_height=$(echo "ibase=16; $(echo $(curl --connect-timeout ${TO} -s -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":83}' ${ETH_NODE_RPC} | jq -r '.result' | cut -c 3-) | tr [:lower:] [:upper:])" | bc)
             if [ -z "${eth_height}" ]; then eth_height=-1; fi
-            valset_cur_nonce=$(curl -s ${NODE_API}/gravity/v1beta/valset/current | jq -r '.valset.nonce')
+            peggo_metrics="eth_height=$eth_height"
+
+            # Get peggo current valset nonce
+            valset_cur_nonce=$(curl --connect-timeout ${TO} -s ${NODE_API}/gravity/v1beta/valset/current | jq -r '.valset.nonce')
             if [ -z "${valset_cur_nonce}" ]; then valset_cur_nonce=-1; fi
-            if [ -z $(curl -s ${NODE_API}/gravity/v1beta/valset/current | jq -r '.valset.members[].ethereum_address'  | grep ${ETH_BR_ADDR}) ]
+            peggo_metrics="$peggo_metrics,valset_cur_nonce=$valset_cur_nonce"
+
+            # Check our bridge address in current valset
+            if [ -z $(curl --connect-timeout ${TO} -s ${NODE_API}/gravity/v1beta/valset/current | jq -r '.valset.members[].ethereum_address' | grep ${ETH_BR_ADDR}) ]
             then
                 valset_cur_ok=false
             else
                 valset_cur_ok=true
             fi
-            grav_eventnonce=$(curl -s ${NODE_API}/gravity/v1beta/oracle/eventnonce/${COS_BR_ADDR} | jq -r '.event_nonce')
+            peggo_metrics="$peggo_metrics,valset_cur_ok=$valset_cur_ok"
+
+            # Get eventnonce
+            grav_eventnonce=$(curl --connect-timeout ${TO} -s ${NODE_API}/gravity/v1beta/oracle/eventnonce/${COS_BR_ADDR} | jq -r '.event_nonce')
             if [ -z "${grav_eventnonce}" ]; then grav_eventnonce=-1; fi
-            logentry="$logentry,eth_height=$eth_height,valset_cur_nonce=$valset_cur_nonce,valset_cur_ok=$valset_cur_ok,grav_eventnonce=$grav_eventnonce"
-				    
-            # Get our validator status
+            peggo_metrics="$peggo_metrics,grav_eventnonce=$grav_eventnonce"
+            logentry="$logentry,$peggo_metrics"
+
+            #
+            # PFD metrics
+            #
+            # Get missed counter
+            pfd_miss_cnt=$(curl --connect-timeout ${TO} -s ${NODE_API}/umee/oracle/v1/validators/${COS_VALOPER}/miss | jq -r '.miss_counter')
+            if [ -z "${pfd_miss_cnt}" ]; then pfd_miss_cnt=-1; fi
+            pfd_metrics="pfd_miss_cnt=$pfd_miss_cnt"
+            # Get UMEE price
+            pfd_ex_rate_UMEE=$(curl --connect-timeout ${TO} -s ${NODE_API}/umee/oracle/v1/denoms/exchange_rates/ | jq '.exchange_rates[] | select(.denom=="UMEE")' | jq -r '.amount')
+            if [ -z "${pfd_ex_rate_UMEE}" ]; then pfd_ex_rate_UMEE=-1; fi
+            pfd_metrics="$pfd_metrics,pfd_ex_rate_UMEE=$pfd_ex_rate_UMEE"
+
+            logentry="$logentry,$pfd_metrics"
+            #
+            # Get our validator metrics
+            #
             if [ -n "${COS_VALOPER}" ]
             then
-                val_status=$(${COS_BIN_NAME} query staking validator ${COS_VALOPER} --output json --node "tcp://localhost:${COS_PORT_RPC}")
+                val_status=$(${COS_BIN_NAME} query staking validator ${COS_VALOPER} --output json --node "${NODE_RPC}")
             fi
+            # Parse validator status
             if [ -n "$val_status" ]
             then
                 jailed=$(jq -r '.jailed' <<<$val_status)
                 # Get all delegated to node tokens num
                 delegated=$(jq -r '.tokens' <<<$val_status)
-                # Get bonded status            
-                if [ $(jq -r '.status' <<<$val_status) == "BOND_STATUS_BONDED" ]; then bonded=true; else bonded=false; fi
-                # Missing blocks number in window (in UMEE slashing window size 100 blocks)
-                bl_missed=$(jq -r '.missed_blocks_counter' <<<$($COS_BIN_NAME q slashing signing-info $($COS_BIN_NAME tendermint show-validator) -o json --node "tcp://localhost:${COS_PORT_RPC}"))
-                # Get validator statistic
-                # Our stake value rank (if not in list assign -1 value)
-                val_rank=$(${COS_BIN_NAME} q staking validators -o json --limit=${list_limit} --node "tcp://localhost:${COS_PORT_RPC}" | \
+                # Get bond status
+                bond=3
+                if [ $(jq -r '.status' <<<$val_status) == "BOND_STATUS_UNBONDED" ]; then bond=2; fi
+                if [ $(jq -r '.status' <<<$val_status) == "BOND_STATUS_UNBONDING" ]; then bond=1; fi
+                if [ $(jq -r '.status' <<<$val_status) == "BOND_STATUS_BONDED" ]; then bond=0; fi
+                # Missing blocks number in window
+                bl_missed=$(jq -r '.missed_blocks_counter' <<<$($COS_BIN_NAME q slashing signing-info $($COS_BIN_NAME tendermint show-validator) -o json --node "${NODE_RPC}"))
+                if [ -z "${bl_missed}" ]; then bl_missed=-1; fi
+                # Our validator stake value rank (if not in list assign -1 value)
+                val_rank=$(${COS_BIN_NAME} q staking validators -o json --limit=${list_limit} --node "${NODE_RPC}" | \
                 jq '.validators[] | select(.status=="BOND_STATUS_BONDED")' | jq -r '.tokens + " - " + .operator_address'  | sort -gr | nl |\
                 grep  "${COS_VALOPER}" | awk '{print $1}')
                 if [ -z "$val_rank" ]; then val_rank=-1; fi
-                logentry="$logentry,jailed=$jailed,delegated=$delegated,bonded=$bonded,bl_missed=$bl_missed,val_rank=$val_rank"
+                logentry="$logentry,jailed=$jailed,delegated=$delegated,bond=$bond,bl_missed=$bl_missed,val_rank=$val_rank"
             else 
                 health=3 # validator status problem
-            fi				
-				fi # MON_MODE
+            fi
+        fi # MON_MODE
         echo "$logentry,health=$health $now"
-        #echo "$logentry_header $logentry,health=$health $now"
     fi # umee rpc check
 fi # umee binary check
--- monitor_bal.sh
+++ monitor_bal.sh
@@ -10,13 +10,13 @@ now=$(date +%s%N)
 logentry="umee_balance"
 if [ -n "${COS_VALOPER}" ]; then logentry=$logentry",valoper=${COS_VALOPER}"; fi
 
-commission=$(${COS_BIN_NAME} query distribution commission ${COS_VALOPER} -o json --node "tcp://127.0.0.1:${COS_PORT_RPC}"| jq -r ' .commission[]  | select(.denom=="'${COS_DENOM}'") | .amount | tonumber')
+commission=$(${COS_BIN_NAME} query distribution commission ${COS_VALOPER} -o json --node "${NODE_RPC}"| jq -r ' .commission[]  | select(.denom=="'${COS_DENOM}'") | .amount | tonumber')
 if [ -z "${commission}" ]; then commission=-1; fi
-rewards=$(${COS_BIN_NAME} query distribution rewards ${COS_WALADDR} ${COS_VALOPER} --node "tcp://127.0.0.1:${COS_PORT_RPC}" -o json | jq  '.rewards[] | select(.denom=="'${COS_DENOM}'") | .amount | tonumber')
+rewards=$(${COS_BIN_NAME} query distribution rewards ${COS_WALADDR} ${COS_VALOPER} --node "${NODE_RPC}" -o json | jq  '.rewards[] | select(.denom=="'${COS_DENOM}'") | .amount | tonumber')
 if [ -z "${rewards}" ]; then rewards=-1; fi
-balance=$(${COS_BIN_NAME} q bank balances ${COS_WALADDR} --node "tcp://127.0.0.1:${COS_PORT_RPC}" -o json | jq  '.balances[] | select(.denom=="'${COS_DENOM}'") | .amount | tonumber')
+balance=$(${COS_BIN_NAME} q bank balances ${COS_WALADDR} --node "${NODE_RPC}" -o json | jq  '.balances[] | select(.denom=="'${COS_DENOM}'") | .amount | tonumber')
 if [ -z "${balance}" ]; then balance=-1; fi
-balance_bridge_umee=$(${COS_BIN_NAME} q bank balances ${COS_BR_ADDR} --node "tcp://127.0.0.1:${COS_PORT_RPC}" -o json | jq  '.balances[] | select(.denom=="'${COS_DENOM}'") | .amount | tonumber')
+balance_bridge_umee=$(${COS_BIN_NAME} q bank balances ${COS_BR_ADDR} --node "${NODE_RPC}" -o json | jq  '.balances[] | select(.denom=="'${COS_DENOM}'") | .amount | tonumber')
 if [ -z "${balance_bridge_umee}" ]; then balance_bridge_umee=-1; fi
 balance_bridge_eth=$(curl -s "http://api.etherscan.io/api?module=account&action=balance&address=${ETH_BR_ADDR}&tag=latest" | jq -r '.result')
 if [ -z "${balance_bridge_eth}" ]; then balance_bridge_eth=-1; fi
